bf910a20dc5c1588a1be8e814a07f9184f2df659
Alarix <117510712+Alarix404@users.noreply.github.com>
2022-11-21 12:59:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const aoijs = require("aoi.js")
 
+const bot = new aoijs.AoiClient({
+  token: process.env.TOKEN,
+  prefix: "!",
+  intents: "all"
+})
+
+//Events
+bot.onMessage()
+
+//Command Example (ping)
+bot.command({
+name: "ping",
+code: `Pong! $pingms`
+})
+
+//Ready Event
+bot.readyCommand({
+    channel: "",
+    code: `$log[Ready on $userTag[$clientID]]`
+})
